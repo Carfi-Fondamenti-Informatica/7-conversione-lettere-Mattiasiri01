ca7a853f8e583da45621c0c13616bc01e876c2ad
Mattiasiri01 <100609727+Mattiasiri01@users.noreply.github.com>
2022-05-07 12:00:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
-
 int main(){
-  return 0;
-}
+    char c;
+    cin>>c;
+    int a= (int)c;
+    bool x=caratteri(a);
+
+    if(x==1){
+        if(65<= a && a<=90){
+            a=a+32;
+            cout<< (char)a<<endl;
+        }
+        else if(97<=a && a<=122){
+            a=a-32;
+            cout<<(char)a<<endl;
+        }
+        else {
+            cout<<"errore";
+        }
+    }
+    return 0;
+    }
